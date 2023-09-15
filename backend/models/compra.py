49103f7d9dee451145579c419e52832541752b07
Andres B <andresblasiche@gmail.com>
2023-09-14 22:56:19 -0300

    Se crea directorio y archivos para los modelos de clases

---
+++ backend/models/compra.py
@@ -0,0 +1,19 @@
+class Compra:
+	fecha = ""
+	total = ""
+
+	def __init__(self, fecha, total):
+		self.fecha = fecha
+		self.total = total
+
+	def get_fecha(self):
+		pass
+
+	def set_fecha(self):
+		pass
+
+	def get_total(self):
+		pass
+
+	def set_total(self):
+		pass
