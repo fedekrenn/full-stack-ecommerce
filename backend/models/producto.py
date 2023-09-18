7a14f4a5b094d8ce82b6cc4449122eba37727b47
Andres B <andresblasiche@gmail.com>
2023-09-17 22:42:04 -0300

    Se completaron los atributos y metodos de la clase

--- backend/models/producto.py
+++ backend/models/producto.py
@@ -1,4 +1,5 @@
 class Producto:
+	n_producto = 0
 	marca = ""
 	rodado = ""
 	precio = ""
@@ -6,7 +7,8 @@ class Producto:
 	estilo = ""
 	material = ""
 
-	def __init__(self, marca, rodado, precio, color, estilo, material):
+	def __init__(self, n_producto, marca, rodado, precio, color, estilo, material):
+		self.n_producto = n_producto
 		self.marca = marca
 		self.rodado = rodado
 		self.precio = precio
@@ -14,38 +16,44 @@ class Producto:
 		self.estilo = estilo
 		self.material = material
 
-	def get_marca(self):
-		pass
+	def get_n_producto(self):
+		return self.n_producto
 
-	def set_marca(self):
-		pass
+	def set_n_producto(self, n_producto):
+		self.n_producto = n_producto
+
+	def get_marca(self):
+		return self.marca
+
+	def set_marca(self, marca):
+		self.marca = marca
 
 	def get_rodado(self):
-		pass
+		return self.rodado
 
-	def set_rodado(self):
-		pass
+	def set_rodado(self, rodado):
+		self.rodado = rodado
 
 	def get_precio(self):
-		pass
+		return self.precio
 
-	def set_precio(self):
-		pass
+	def set_precio(self, precio):
+		self.precio = precio
 
 	def get_color(self):
-		pass
+		return self.color
 
-	def set_color(self):
-		pass
+	def set_color(self, color):
+		self.color = color
 
 	def get_estilo(self):
-		pass
+		return self.estilo
 
-	def set_estilo(self):
-		pass
+	def set_estilo(self, estilo):
+		self.estilo = estilo
 
 	def get_material(self):
-		pass
+		return self.material
 
-	def set_material(self):
-		pass
+	def set_material(self, material):
+		self.material = material
