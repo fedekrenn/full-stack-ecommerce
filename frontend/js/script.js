276772744bb77b754cbf4785df7ed1ecf4864a23
Fernando Barletta <ferbarletta@gmail.com>
2023-10-04 20:55:49 -0300

    se agragan validaciones de login en la page login.html

---
+++ frontend/js/script.js
@@ -0,0 +1,13 @@
+function ValidarUsuario(){
+    if (document.getElementById("email").value != '' && document.getElementById("password").value != ''){
+        if (document.getElementById("email").value == 'ivana@gmail.com') {
+            if (document.getElementById("password").value == '123'){   
+                alert('Bienvenica Ivana')            
+               }
+            else {alert("Contraseña incorrecta")}   
+            }
+        else {
+            alert("Corrreo electrónico incorrecto")
+            }
+    }
+}
