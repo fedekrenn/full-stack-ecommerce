8555969547237298639c00dd26098b6a3bbee2d5
Fernanda Castillo <fernandaaracelicastillo@gmail.com>
2023-10-10 21:45:00 -0300

    Se crean las clases  de acceso a la db

--- backend/data_access/producto.py
+++ backend/data_access/producto.py
@@ -1,1 +1,20 @@
-# A realizar
+class ProductoDao():
+
+    def __init__(self, db):
+        self.db = db
+        self.cursor = db.cursor()
+
+    def obtener_producto_id(self, id_producto):
+        pass
+
+    def obtener_todos_produdctos(self):
+        pass
+
+    def crear_pruducto(self, producto):
+        pass
+
+    def editar_producto(self, id_producto, producto):
+        pass
+
+    def eliminar_producto(self, id_producto):
+        pass
