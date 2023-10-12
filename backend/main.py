e1a9e392b690517293ace51ba11f4f7a19809ee7
Federico Krenn <fedekrenn@gmail.com>
2023-10-12 18:40:24 -0300

    Refactorización de últimos detalles

--- backend/main.py
+++ backend/main.py
@@ -4,3 +4,10 @@ from data_access.producto import *
 dao = ProductoDao(dbdata)
 
 print(dao.obtener_todos_productos())
+
+producto_prueba = Producto('Bicicleta de prueba', 15000, 10, 'Zapatilla Nike Air Max 90', 1, 1, 1, 1)
+
+print(dao.crear_producto(producto_prueba))
+
+# El de abajo debería fallar porque no es una instancia de Producto
+print(dao.crear_producto('Este debería fallar', 15000, 10, 'Zapatilla Nike Air Max 90', 1, 1, 1, 1))
