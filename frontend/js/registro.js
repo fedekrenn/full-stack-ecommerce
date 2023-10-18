e4dd7e548d5c2d2a4434bc56fdb3eeb664616c73
Fede Krenn <fedekrenn@gmail.com>
2023-10-18 01:00:31 -0300

    Se aplican correciones del validador w3c

--- frontend/js/registro.js
+++ frontend/js/registro.js
@@ -1,17 +1,19 @@
-const pass1 = document.getElementById("pass1");
-const pass2 = document.getElementById("pass2");
+const pass1 = document.getElementById('pass1')
+const pass2 = document.getElementById('pass2')
+const form = document.getElementById('form')
 
 function validarClaves() {
-	if (pass1.value != "" && pass2.value != "") {
-		if (pass1.value == pass1.value) {
-			//Hacer algo
-		}else{
-				alert("Las contraseñas no coinciden");
-		}
-	}
+  if (pass1.value != '' && pass2.value != '') {
+    if (pass1.value == pass2.value) {
+      alert('El registro se ha realizado con éxito')
+			window.location.href = "./login.html";
+    } else {
+      alert('Las contraseñas no coinciden')
+    }
+  }
 }
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	validarClaves();
-});
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  validarClaves()
+})
