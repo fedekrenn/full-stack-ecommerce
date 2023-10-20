a550b6d37e709bfba889d37649f4bf1d9da1c9cd
lautaro <lnancillotti@gmail.com>
2023-10-20 00:32:16 -0300

    Se corrige la landing page

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -4,13 +4,16 @@ const form = document.getElementById("form");
 const nombre = document.getElementById("nombre");
 var contador = 0;
 
+const usuarioAdmin = "admin@admin.com"
+const passAdmin = 'admin'
+
 function validarUsuario() {
   if (email.value != "" && password.value != "") {
     contador = contador + 1;
     if (contador < 3) {
-      if (email.value == "ivana@gmail.com") {
-        if (password.value == "123") {
-          alert ("Bienvenida "  + email.value)
+      if (email.value == usuarioAdmin) {
+        if (password.value == passAdmin) {
+          alert ("Bienvenido "  + email.value)
           window.location.href = "../index.html";
         } else {
           alert("Contraseña incorrecta");
